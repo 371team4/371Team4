942fb9ad914af0c14605a5d07e85d3c4ca4c4c66
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-28 05:03:41 -0600

    some dummy routes

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,20 @@
 <template>
   <v-app light>
-    <v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" app>
-      <v-list>
-        <v-list-tile value="true" v-for="(item, i) in items" :key="i" exact>
-          <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn icon light @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon light @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon light @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon light @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-    <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed>
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
+    <v-tabs v-model="active">
+      <v-tabs-bar class="cyan" dark>
+        <v-tabs-item v-for="tab in tabs" :key="tab" :href="'#' + tab" ripple>
+          <router-link :to="{name: tab}">{{tab}}</router-link>
+        </v-tabs-item>
+      </v-tabs-bar>
+      <v-tabs-items>
+        <v-tabs-content v-for="tab in tabs" :key="tab" :id="tab">
+          <v-card flat>
+            <v-card-text>{{ tab }}</v-card-text>
+          </v-card>
+        </v-tabs-content>
+      </v-tabs-items>
+    </v-tabs>
+    <router-view></router-view>
   </v-app>
 </template>
 
@@ -52,20 +22,9 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      tabs: ['ShowView', 'Projects', 'Designer', 'Calendar']
     }
   }
+
 }
 </script>
