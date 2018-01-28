fd377a922b34583442769f150b960cda028eb6ea
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-28 11:40:27 -0600

    add firebase config and service. don't forgot to redirect the routes

---
+++ src/services/firebase.conf.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBT7DMm3c5lM6QyoTL11B5yi6tzVKXbrSY",
+  authDomain: "repartyed.firebaseapp.com",
+  databaseURL: "https://repartyed.firebaseio.com",
+  projectId: "repartyed",
+  storageBucket: "repartyed.appspot.com",
+  messagingSenderId: "205921360328"
+}
+
+firebase.initializeApp(config)
+export default {
+  db : firebase.database(),
+  dbRef: db.ref('/repartyed'),
+  auth: firebase.auth()
+}
