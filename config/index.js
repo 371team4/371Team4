33b7ddc239c8cb5dffbe19705b8d57680da7a5ce
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-28 02:53:39 -0600

    upgrade all packages

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 'use strict'
-// Template version: 1.2.8
+// Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
 
 module.exports = {
   dev: {
-    env: require('./dev.env'),
+
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -15,7 +15,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
+    autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -42,8 +42,8 @@ module.exports = {
 
     cssSourceMap: true
   },
+
   build: {
-    env: require('./prod.env'),
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
 
