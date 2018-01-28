e3935e3319e74ac0c067d8e067ae333ab4a2926b
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-28 10:27:11 -0600

    remove console exception

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <v-app light>
-    <v-tabs v-model="active">
+    <v-tabs>
       <v-tabs-bar class="cyan" dark>
         <v-tabs-item v-for="tab in tabs" :key="tab" :href="'#' + tab" ripple>
           <router-link :to="{name: tab}">{{tab}}</router-link>
         </v-tabs-item>
       </v-tabs-bar>
       <v-tabs-items>
-        <v-tabs-content v-for="tab in tabs" :key="tab" :id="tab">
-          <v-card flat>
-            <v-card-text>{{ tab }}</v-card-text>
-          </v-card>
-        </v-tabs-content>
+        <router-view></router-view>
       </v-tabs-items>
     </v-tabs>
-    <router-view></router-view>
   </v-app>
 </template>
 
