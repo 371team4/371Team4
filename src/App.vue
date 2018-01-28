e9f69a8b59964162c00c3327b8cd5104c64029a2
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-28 01:36:05 -0600

    we can compile now, formatter and linter are working good

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
+    <v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" app>
       <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-          exact
-        >
+        <v-list-tile value="true" v-for="(item, i) in items" :key="i" exact>
           <v-list-tile-action>
             <v-icon light v-html="item.icon"></v-icon>
           </v-list-tile-action>
@@ -25,46 +14,25 @@
     </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn
-        icon
-        light
-        @click.stop="miniVariant = !miniVariant"
-      >
+      <v-btn icon light @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn
-        icon
-        light
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon light @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        light
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon light @click.stop="fixed = !fixed">
         <v-icon>remove</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        icon
-        light
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <v-btn icon light @click.stop="rightDrawer = !rightDrawer">
         <v-icon>menu</v-icon>
       </v-btn>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
+    <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed>
       <v-list>
         <v-list-tile @click="right = !right">
           <v-list-tile-action>
@@ -87,10 +55,12 @@
         clipped: false,
         drawer: true,
         fixed: false,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
+        items: [
+          {
+            icon: 'bubble_chart',
+            title: 'Inspire'
+          }
+        ],
         miniVariant: false,
         right: true,
         rightDrawer: false,
