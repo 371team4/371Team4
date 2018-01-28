21a59711a000736b47a98d2362e74a9643184c24
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-28 11:40:27 -0600

    add firebase config and service. don't forgot to redirect the routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,18 @@ import ShowView from '@/components/ShowView'
 import Projects from '@/components/Projects'
 import Designer from '@/components/Designer'
 import Calendar from '@/components/Calendar'
+import SignIn from '@/components/SignIn'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '*',
+      name: 'Sign in',
+      component: SignIn
+    },
     {
       path: '/',
       name: 'ShowView',
