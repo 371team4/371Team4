ed0a017047e538bed3a16c005cd162e902e5f458
KingMackle <mahmoud.mahmoud@usask.ca>
2018-01-29 12:50:25 -0600

    3, 2, 1 we have signup. Basic sign up, very, very basic

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -10,7 +9,6 @@ import router from './router'
 import { store } from './store'
 
 Vue.use(Vuetify)
-Vue.use(Vuex)
 Vue.config.silent = false
 Vue.config.performance = true
 Vue.config.productionTip = true
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,61 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
+import { authService } from '../services/firebase.conf'
+
+Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    _isTrue: false
-  },
-  getters: {
-    isTrue (state) {
-      return state._isTrue
-    }
-  },
-  mutations: {
-    toggle (state, newVal) {
-      state._isTrue = newVal
-    }
+    user: null
   },
   actions: {
-
+    signUserUp ({ commit }, payload) {
+      authService.createUserWithEmailAndPassword(payload.userid, payload.password)
+        .then(
+          user => {
+            const newUser = {
+              id: user.uid,
+              name: user.email,
+              userid: user.id
+            }
+            debugger
+            commit('setUser', { 'user': newUser })
+          }
+        )
+        .catch(
+          error => {
+            console.log(error)
+          }
+        )
+    }
+  },
+  signUserIn ({ commit }, payload) {
+    authService.signInWithEmailAndPassword(payload.userid, payload.password)
+      .then(
+        user => {
+          const newUser = {
+            id: user.uid,
+            name: user.email,
+            userid: user.id
+          }
+          debugger
+          commit('setUser', { 'user': newUser })
+        }
+      )
+      .catch(
+        error => {
+          console.log(error)
+        }
+      )
+  },
+  mutations: {
+    setUser (state, payload) {
+      state.user = payload.user
+    }
+  },
+  getters: {
+    user (state) {
+      return state.user
+    }
   }
 })
