d49b38e1a0bdc2e3f7a6cb47ff03f98636cd523d
Amy Kwan (amk899) <amy.kwan@usask.ca>
2018-02-15 13:55:05 -0600

    Edited grid layout to position items on the page more equally

--- src/components/Templates/SlideTemplate1.vue
+++ src/components/Templates/SlideTemplate1.vue
@@ -1,31 +1,59 @@
 <template>
-  <v-container>
-    <v-layout
-      row
-      wrap>
-      <h1>hi</h1>
-    </v-layout>
-
-    <!-- Carousel Component -->
+  <v-container
+    row
+    wrap
+    fluid
+    grid-list-lg>
     <v-layout
       row
       wrap>
       <v-flex
-      xs12>
+        d-flex
+        xs12
+        sm6
+        md6
+        lg6>
+        <v-card
+          color="light grey"
+          dark>
+          <v-card-title
+            primary
+            class="title"> Hi
+          </v-card-title>
+        </v-card>
+      </v-flex>
 
+      <!-- Carousel Component -->
+      <v-flex
+        d-flex
+        xs12
+        sm6
+        md6
+        lg6>
         <v-carousel
           hide-controls
           hide-delimiters>
           <v-carousel-item
             v-for="(item,i) in items"
             :src="item.src"
-            :key="i">
-          </v-carousel-item>
+            :key="i"/>
         </v-carousel>
-
       </v-flex>
     </v-layout>
     <!-- Done the Carousel item -->
+    <v-jumbotron color="grey lighten-2">
+      <v-container fill-height>
+        <v-layout align-center>
+          <v-flex>
+            <h3 class="display-3">Title is here</h3>
+            <span class="subheading">Insert body here</span>
+            <v-divider class="my-3"/>
+            <div class="title mb-3">Check out our newest features!</div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-jumbotron>
+
   </v-container>
 </template>
 
