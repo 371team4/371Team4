cfb8655de1d7b0688ff8e6710ce2ce5d93141cad
Benjamin Korol (bek082) <ben.korol@usask.ca>
2018-02-15 11:47:24 -0600

    added component template TemplateCardButton.vue

---
+++ src/components/Templates/TemplateCardButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+    height="200px"
+    @click="clickFunc()"
+    v-model="isVisible"
+    disabled="isDisabled">
+    <v-btn>
+      <v-icon>add</v-icon>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    isVisible: {
+      type: Boolean,
+      required: true
+    },
+    isDisabled: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  methods: {
+    clickFunc: function () {
+      this.$emit('ButtonClick')
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
