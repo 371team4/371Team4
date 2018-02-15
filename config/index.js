bf3c6a8f3aef6b04882c7bd7db759f2247e48a86
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-02-14 18:16:26 -0600

    force poll option to hot reload on mac, force chrome as the dev browser

--- config/index.js
+++ config/index.js
@@ -15,10 +15,10 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: true,
+    autoOpenBrowser: 'chrome',
     errorOverlay: true,
     notifyOnErrors: true,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    poll: true, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
