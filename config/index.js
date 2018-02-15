7ca4ac9075a063f1c41da97bb14487ebc643b870
KingMackle <mahmoud.mahmoud@usask.ca>
2018-02-15 11:07:50 -0600

    force use of google chrome for development all the time

--- config/index.js
+++ config/index.js
@@ -4,6 +4,26 @@
 
 const path = require('path')
 
+// get the browser command, specifically google chrome
+const browserCommand = getBrowserCommand()
+
+// return browser command for the given operating system
+function getBrowserCommand () {
+  // get the operating system version that we are running on
+  const platform = process.platform
+  if (platform === 'darwin') {// if we are running on mac os
+    return 'Chrome'
+  } else if (platform === 'win32'){ // if we are running on windows
+    return 'chrome'
+  } else if (platform === 'linux') { // if we are running on linux
+    return 'google-chrome'
+  } else {
+    // if the operating system is something that is not configured above
+    // then just open the default browser 
+    return true
+  }
+}
+
 module.exports = {
   dev: {
 
@@ -15,7 +35,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: 'chrome',
+    autoOpenBrowser: browserCommand,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: true, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
