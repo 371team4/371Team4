7f23ab7f84bdfa7a40d743e2aeb16dbc781a8168
Benjamin Korol (bek082) <ben.korol@usask.ca>
2018-02-15 11:40:08 -0600

    added component AddButton.vue

---
+++ src/components/shared/AddButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+  height="200px"
+  v-on:click="add()"
+  v-model="isVisible"
+  disabled="isDisabled">
+    <v-btn>
+      <v-icon>add</v-icon>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props:{
+    isVisible:{
+      type: Boolean,
+      required: true
+    },
+    isDisabled:{
+      type: Boolean,
+      required: true
+    }
+  },
+
+  methods: {
+    add: function(){
+      this.$emit('cButtonClick')
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
