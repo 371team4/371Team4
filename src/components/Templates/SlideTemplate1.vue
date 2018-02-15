743cddc3e3d1a16ff54e9a0a79fd7fc5439e97f5
Amy Kwan (amk899) <amy.kwan@usask.ca>
2018-02-15 10:37:11 -0600

    Added the carousel component for the slide template

--- src/components/Templates/SlideTemplate1.vue
+++ src/components/Templates/SlideTemplate1.vue
@@ -1,14 +1,44 @@
 <template>
   <v-container>
-    <v-layout>
-      <h1>"hello world"</h1>
+    <v-layout
+      row
+      wrap>
+      <h1>hi</h1>
     </v-layout>
+
+    <!-- Carousel Component -->
+    <v-layout
+      row
+      wrap>
+      <v-flex
+      xs12>
+
+        <v-carousel
+          hide-controls
+          hide-delimiters>
+          <v-carousel-item
+            v-for="(item,i) in items"
+            :src="item.src"
+            :key="i">
+          </v-carousel-item>
+        </v-carousel>
+
+      </v-flex>
+    </v-layout>
+    <!-- Done the Carousel item -->
   </v-container>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      items: [
+        { src: 'https://www.usask.ca/programs/images/college.jpg', id: '1' },
+        { src: 'http://csee2015.usask.ca/images/3611278449_e68f87520b_b.jpg', id: '2' }
+      ]
+    }
+  }
 }
 </script>
 
