2bcc4ff231937c4d3b995c0e3adee2408635d46e
Ridwan <ror716@mail.usask.ca>
2018-02-15 23:29:28 -0600

    create all the mutation types

--- src/store/modules/upload/index.js
+++ src/store/modules/upload/index.js
@@ -1,4 +1,8 @@
 // mutation types
+const SET_BYTES_UPLOADED = 'SET_BYTES_UPLOADED'
+const SET_BYTES_REMAINING = 'SET_BYTES_REMAINING'
+const SET_IS_UPLOADING = 'SET_IS_UPLOADING'
+const SET_CANCEL_UPLOAD = 'SET_CANCEL_UPLOAD'
 
 // state of this module
 const state = {
@@ -12,7 +16,18 @@ const getters = {
 
 // mutations of this module, mutation must be sync and atomic
 const mutations = {
-
+  [SET_BYTES_UPLOADED] (state, payload) {
+    state.bytesUploaded = payload.bytesUploaded
+  },
+  [SET_BYTES_REMAINING] (state, payload) {
+    state.bytesRemaining = payload.bytesRemaining
+  },
+  [SET_IS_UPLOADING] (state, payload) {
+    state.isUploading = payload.isUploading
+  },
+  [SET_CANCEL_UPLOAD] (state, payload) {
+    state.cancelUpload = payload.cancelUpload
+  }
 }
 
 // actions can be async and may have side effects
