43e536dcad402e005d8ba53208eb48182032898f
Ridwan <ror716@mail.usask.ca>
2018-02-16 00:36:40 -0600

    upload.vue component to test upload and cancel upload | pair with @yul040 (Li)

---
+++ src/components/Upload.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input
+      accept="image/*"
+      type="file"
+      @change="uploadAndSubmit">
+    <button
+    @click="cancelUpload">cancel</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      files: []
+    }
+  },
+  methods: {
+    uploadAndSubmitOne: function ({ target }) {
+      let tmpArray = [...target.files]
+      this.files = tmpArray.filter(file => file.type.indexOf('image/') !== -1)
+      this.$store.dispatch('uploadSingleFile', tmpArray[0])
+    },
+    cancelUpload: function () {
+      this.$store.dispatch('cancelUpload', true)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
