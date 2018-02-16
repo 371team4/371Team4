750e8fe667de2a7e46b2df8c5ec84d7eed4d4350
Ridwan <ror716@mail.usask.ca>
2018-02-16 00:06:37 -0600

    Add cancelUpload to uploadFile action | pair work with @yul040 (Li)

--- src/store/modules/upload/index.js
+++ src/store/modules/upload/index.js
@@ -1,3 +1,6 @@
+import { storageDB } from '@/services/firebase.conf'
+import Firebase from 'firebase'
+
 // mutation types
 const SET_BYTES_UPLOADED = 'SET_BYTES_UPLOADED'
 const SET_BYTES_REMAINING = 'SET_BYTES_REMAINING'
@@ -6,12 +9,18 @@ const SET_CANCEL_UPLOAD = 'SET_CANCEL_UPLOAD'
 
 // state of this module
 const state = {
-  cancelUpload: false
+  cancelUpload: false,
+  isUploading: false,
+  bytesUploaded: 0,
+  bytesRemaining: 0
 }
 
 // getters for this module's state
 const getters = {
-  getCancelUpload: state => state.cancelUpload
+  getCancelUpload: state => state.cancelUpload,
+  getIsUploading: state => state.isUploading,
+  getBytesUploaded: state => state.bytesUploaded,
+  getBytesRemaining: state => state.bytesRemaining
 }
 
 // mutations of this module, mutation must be sync and atomic
@@ -35,6 +44,45 @@ const actions = {
   // cancel upload action
   cancelUpload (state, payload) {
     state.commit(SET_CANCEL_UPLOAD, payload)
+  },
+  // action to upload file
+  uploadSingleFile (context, file) {
+    // pre-set upload status
+    context.commit(SET_IS_UPLOADING, true)
+    context.commit(SET_BYTES_UPLOADED, 0)
+    context.commit(SET_BYTES_REMAINING, file.size)
+    // start upload
+    var upload = storageDB.child('files/' + file.name).put(file)
+    upload.on(Firebase.storage.TaskEvent.STATE_CHANGED, function (snapshot) {
+      // update upload status
+      context.commit(SET_BYTES_UPLOADED, snapshot.bytesTransferred)
+      context.commit(SET_BYTES_REMAINING, snapshot.totalBytes - snapshot.bytesTransferred)
+      // check if user cancel upload
+      if (context.state.cancelUpload) {
+        alert('upload canceled')
+        upload.cancel()
+        context.commit(SET_IS_UPLOADING, false)
+        context.commit(SET_BYTES_UPLOADED, 0)
+        context.commit(SET_BYTES_REMAINING, 0)
+        context.commit(SET_CANCEL_UPLOAD, false)
+      }
+    }, function (error) {
+      // if upload error
+      alert(error.code)
+      context.commit(SET_IS_UPLOADING, false)
+      context.commit(SET_BYTES_UPLOADED, 0)
+      context.commit(SET_BYTES_REMAINING, 0)
+      context.commit(SET_CANCEL_UPLOAD, false)
+    }, function () {
+      // if upload success
+      alert('upload success')
+      context.commit(SET_IS_UPLOADING, false)
+      context.commit(SET_BYTES_UPLOADED, 0)
+      context.commit(SET_BYTES_REMAINING, 0)
+      context.commit(SET_CANCEL_UPLOAD, false)
+      // store it to event so load event directly use this url not use download api
+      // console.log(upload.snapshot.downloadURL)
+    })
   }
 }
 
