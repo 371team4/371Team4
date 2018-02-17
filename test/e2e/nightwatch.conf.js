0a69fb0478fdde3f22e0efcba0f8dda0d18aa957
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-02-17 11:27:00 -0600

    starter for page objects

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -6,6 +6,7 @@ module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
   custom_assertions_path: ['test/e2e/custom-assertions'],
+  page_objects_path: ['test/e2e/pageObj'],
 
   selenium: {
     start_process: true,
