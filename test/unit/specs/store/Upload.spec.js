f8efe4b0caf753d9a486ab96e9844a26d8fc70e1
Ridwan <ror716@mail.usask.ca>
2018-02-25 21:38:43 -0600

    Move directory of cancelUpload unit test from ... test/unit/specs/Upload.spec.js to test/unit/specs/store/Upload.spec.js

--- test/unit/specs/store/Upload.spec.js
+++ test/unit/specs/store/Upload.spec.js
@@ -1,5 +1,6 @@
-import index from '../../../../src/store/modules/upload/index'
+import index from '@/store/modules/upload/index'
 
+// helper for testing action with expected mutations
 const testAction = (action, args, state, expectedMutations, done) => {
   let count = 0
 
@@ -22,34 +23,29 @@ const testAction = (action, args, state, expectedMutations, done) => {
     }
   }
 
-  // call action with mock commit and args
+  // call the action with mocked store and argument
   action({ commit, state }, ...args)
 
-  // check if mutation is dispatched
+  // Check if no mutation should have been dispatched
   if (expectedMutations.length === 0) {
     expect(count).to.equal(0)
     done()
   }
 }
 
-// create a mock file for test upload
-const str = 'these are the content of this file. It should be a base64 string but will do that later'
-const f = new File([str], 'filename')
-
+// Unit test for cancelUpload
 describe('index', () => {
-  it('uploadSingleFile', done => {
-    testAction(index.actions.uploadSingleFile, [f], {}, [
-      // mutation should been called in order from first to last
-      { type: 'SET_IS_UPLOADING', payload: true },
-      { type: 'SET_BYTES_UPLOADED', payload: 0 },
-      { type: 'SET_BYTES_REMAINING', payload: str.length },
-      { type: 'SET_BYTES_UPLOADED', payload: 0 },
-      { type: 'SET_BYTES_REMAINING', payload: str.length },
-      { type: 'SET_BYTES_UPLOADED', payload: str.length },
-      { type: 'SET_BYTES_REMAINING', payload: 0 },
-      { type: 'SET_IS_UPLOADING', payload: false },
-      { type: 'SET_BYTES_UPLOADED', payload: 0 },
-      { type: 'SET_BYTES_REMAINING', payload: 0 },
+  it('should cancel an Upload', done => {
+    testAction(index.actions.cancelUpload, [true], {}, [
+      { type: 'SET_CANCEL_UPLOAD', payload: true }
+    ], done)
+  })
+})
+
+// Unit test for cancelUpload fail
+describe('index', () => {
+  it('should not cancel an Upload. Cancel upload failed', done => {
+    testAction(index.actions.cancelUpload, [false], {}, [
       { type: 'SET_CANCEL_UPLOAD', payload: false }
     ], done)
   })
