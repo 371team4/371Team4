6f36386aed76afa3e6ddf8e8e8c7a2ce7343990c
peggyopal <peggyopal@users.noreply.github.com>
2018-02-26 20:28:16 -0600

    Revert "create cancelupload action"

--- test/unit/specs/store/Upload.spec.js
+++ test/unit/specs/store/Upload.spec.js
@@ -1,6 +1,5 @@
-import index from '@/store/modules/upload/index'
+import index from '../../../../src/store/modules/upload/index'
 
-// helper for testing action with expected mutations
 const testAction = (action, args, state, expectedMutations, done) => {
   let count = 0
 
@@ -23,29 +22,34 @@ const testAction = (action, args, state, expectedMutations, done) => {
     }
   }
 
-  // call the action with mocked store and argument
+  // call action with mock commit and args
   action({ commit, state }, ...args)
 
-  // Check if no mutation should have been dispatched
+  // check if mutation is dispatched
   if (expectedMutations.length === 0) {
     expect(count).to.equal(0)
     done()
   }
 }
 
-// Unit test for cancelUpload
-describe('index', () => {
-  it('should cancel an Upload', done => {
-    testAction(index.actions.cancelUpload, [true], {}, [
-      { type: 'SET_CANCEL_UPLOAD', payload: true }
-    ], done)
-  })
-})
+// create a mock file for test upload
+const str = 'these are the content of this file. It should be a base64 string but will do that later'
+const f = new File([str], 'filename')
 
-// Unit test for cancelUpload fail
 describe('index', () => {
-  it('should not cancel an Upload. Cancel upload failed', done => {
-    testAction(index.actions.cancelUpload, [false], {}, [
+  it('uploadSingleFile', done => {
+    testAction(index.actions.uploadSingleFile, [f], {}, [
+      // mutation should been called in order from first to last
+      { type: 'SET_IS_UPLOADING', payload: true },
+      { type: 'SET_BYTES_UPLOADED', payload: 0 },
+      { type: 'SET_BYTES_REMAINING', payload: str.length },
+      { type: 'SET_BYTES_UPLOADED', payload: 0 },
+      { type: 'SET_BYTES_REMAINING', payload: str.length },
+      { type: 'SET_BYTES_UPLOADED', payload: str.length },
+      { type: 'SET_BYTES_REMAINING', payload: 0 },
+      { type: 'SET_IS_UPLOADING', payload: false },
+      { type: 'SET_BYTES_UPLOADED', payload: 0 },
+      { type: 'SET_BYTES_REMAINING', payload: 0 },
       { type: 'SET_CANCEL_UPLOAD', payload: false }
     ], done)
   })
