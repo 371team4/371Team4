933414017a8af205b368d5dd787e92ac535f7fa3
Yuecheng Rong (yur013) <yur013@mail.usask.ca>
2018-03-01 21:47:49 -0600

    commit calendar view

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    <h1>Calendar View</h1>
-  </div>
+  <v-container fluid>
+    <full-calendar :events="fcEvents"/>
+  </v-container>
 </template>
 
 <script>
-export default {
 
+export default {
+  components: {
+    'full-calendar': require('vue-fullcalendar')
+  },
+  computed: {
+    fcEvents () {
+      const Slides = this.$store.getters.getSlide
+      const filteredSlides = (Slides.map((slide) => {
+        const newSlide = {}
+        if (slide &&
+            slide.title &&
+            slide.title.content &&
+            slide.date &&
+            slide.date.content) {
+          // Copy the title and start time
+          newSlide.title = slide.title.content
+          newSlide.start = this.addOneDay(slide)
+        }
+        return newSlide
+      }))
+      return filteredSlides
+    }
+  },
+  methods: {
+    // Since the calendar view cannot show the correct date,
+    // One day is added manually
+    addOneDay: function (slide) {
+      var Data = new Date(slide.date.content)
+      Data.setDate(Data.getDate() + 1)
+      return Data
+    }
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
