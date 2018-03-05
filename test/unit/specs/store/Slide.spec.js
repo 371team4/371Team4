d16f5d297a23dd8a9d3ef597540a7a0a73baae66
Benjamin Korol (bek082) <ben.korol@usask.ca>
2018-03-04 19:11:38 -0600

    add time getter tests

--- test/unit/specs/store/Slide.spec.js
+++ test/unit/specs/store/Slide.spec.js
@@ -954,5 +954,94 @@ describe('slide', () => {
         assert(state.isCurrentSlideDirty, true)
       })
     })
+
+    describe('time', () => {
+      let state
+
+      beforeEach(() => {
+        state = refreshState()
+      })
+
+      it('SET_TIME', () => {
+        const titleStateBeforeMutation = {
+          content: null,
+          fontColor: null,
+          fontSize: null,
+          fontStyle: null,
+          fontWeight: null
+        }
+
+        const titleStateAfterMutation = {
+          content: 'Slide1',
+          fontColor: 'Red',
+          fontSize: 'Large',
+          fontStyle: 'Normal',
+          fontWeight: 'Bold'
+        }
+        assert(state.currentSlide.time, titleStateBeforeMutation)
+        assert(state.isCurrentSlideDirty, false)
+
+        slide.mutations[CURRENT_SLIDE.SET_TIME](state, {
+          content: 'Slide1',
+          fontColor: 'Red',
+          fontSize: 'Large',
+          fontStyle: 'Normal',
+          fontWeight: 'Bold'
+        })
+
+        assert(slide.getters.getCurrentSlideTime(state), titleStateAfterMutation)
+        assert(state.isCurrentSlideDirty, true)
+      })
+
+      it('SET_TIME_CONTENT', () => {
+        assert(state.currentSlide.time.content, null)
+        assert(state.isCurrentSlideDirty, false)
+
+        slide.mutations[CURRENT_SLIDE.SET_TIME_CONTENT](state, 'title')
+
+        assert(slide.getters.getCurrentSlideTimeContent(state), 'title')
+        assert(state.isCurrentSlideDirty, true)
+      })
+
+      it('SET_TIME_FONT_COLOR', () => {
+        assert(state.currentSlide.time.fontColor, null)
+        assert(state.isCurrentSlideDirty, false)
+
+        slide.mutations[CURRENT_SLIDE.SET_TIME_FONT_COLOR](state, 'Red')
+
+        assert(slide.getters.getCurrentSlideTimeFontColor(state), 'Red')
+        assert(state.isCurrentSlideDirty, true)
+      })
+
+      it('SET_TIME_FONT_SIZE', () => {
+        assert(state.currentSlide.time.fontSize, null)
+        assert(state.isCurrentSlideDirty, false)
+
+        slide.mutations[CURRENT_SLIDE.SET_TIME_FONT_SIZE](state, 'Large')
+
+        assert(slide.getters.getCurrentSlideTimeFontSize(state), 'Large')
+        assert(state.isCurrentSlideDirty, true)
+      })
+
+      it('SET_TIME_FONT_STYLE', () => {
+        assert(state.currentSlide.time.fontStyle, null)
+        assert(state.isCurrentSlideDirty, false)
+
+        slide.mutations[CURRENT_SLIDE.SET_TIME_FONT_STYLE](state, 'Normal')
+
+        assert(slide.getters.getCurrentSlideTimeFontStyle(state), 'Normal')
+        assert(state.isCurrentSlideDirty, true)
+      })
+
+      it('SET_TIME_FONT_WEIGHT', () => {
+        assert(state.currentSlide.time.fontWeight, null)
+        assert(state.isCurrentSlideDirty, false)
+
+        slide.mutations[CURRENT_SLIDE.SET_TIME_FONT_WEIGHT](state, 'Bold')
+
+        assert(slide.getters.getCurrentSlideTimeFontWeight(state), 'Bold')
+        assert(state.isCurrentSlideDirty, true)
+      })
+    })
   })
 })
