41b0204493dfc61d36cae927023bfaee9278744f
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-05 20:54:49 -0600

    trying to fix running tests in vscode

--- test/e2e/src/test/java/com/reparty/app/AppTest.java
+++ test/e2e/src/test/java/com/reparty/app/AppTest.java
@@ -1,8 +1,16 @@
 package com.reparty.app;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
+  @Test
+  public void test1() {
+    Assert.assertTrue(true);
+    System.out.println("Hello, I was just ran");
+  }
 }
