2f4deccf7d41a6d7b0a70758fb91e36169a0e7d3
RIDWAN RAJI <ror716@RIDWANs-MacBook-Pro.local>
2018-03-11 01:56:05 -0600

    Add $data to test cases

--- test/unit/specs/FontStyle.spec.js
+++ test/unit/specs/FontStyle.spec.js
@@ -6,7 +6,7 @@ describe('FontStyle.vue', () => {
   it('Font Colors should be 11', () => {
     const Constructor = Vue.extend(FontStyle)
     const vm = new Constructor().$mount()
-    expect(vm.fontColors.length).to.equal(11)
+    expect(vm.$data.fontColors.length).to.equal(11)
   })
 })
 
@@ -15,7 +15,7 @@ describe('FontStyle.vue', () => {
   it('Font Sizes should be 9', () => {
     const Constructor = Vue.extend(FontStyle)
     const vm = new Constructor().$mount()
-    expect(vm.fontSizes.length).to.equal(9)
+    expect(vm.$data.fontSizes.length).to.equal(9)
   })
 })
 
@@ -24,7 +24,7 @@ describe('FontStyle.vue', () => {
   it('Font Styles should be 3', () => {
     const Constructor = Vue.extend(FontStyle)
     const vm = new Constructor().$mount()
-    expect(vm.fontStyles.length).to.equal(3)
+    expect(vm.$data.fontStyles.length).to.equal(3)
   })
 })
 
@@ -33,6 +33,6 @@ describe('FontStyle.vue', () => {
   it('Font Weights should be 4', () => {
     const Constructor = Vue.extend(FontStyle)
     const vm = new Constructor().$mount()
-    expect(vm.fontWeights.length).to.equal(4)
+    expect(vm.$data.fontWeights.length).to.equal(4)
   })
 })
