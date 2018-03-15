3580e658addda2761f179e77d38a5787e22384a3
Yuecheng Rong (yur013) <yur013@mail.usask.ca>
2018-03-14 18:59:36 -0600

    new calendar view

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,44 +1,59 @@
 <template>
   <v-container fluid>
-    <full-calendar :events="Events"/>
+    <full-calendar
+      :config="config"
+      :events="events"/>
   </v-container>
 </template>
 
 <script>
-
+// Import calendar as a plugin
+import { FullCalendar } from 'vue-full-calendar'
+// CSS
+import 'fullcalendar/dist/fullcalendar.min.css'
 export default {
+  name: 'Hello',
   components: {
-    'full-calendar': require('vue-fullcalendar')
+    FullCalendar
   },
-  computed: {
-    Events () {
-      return this.$store.getters.getAllSlides.map((slide) => {
-        const newSlide = {}
-        if (slide &&
-            slide.title &&
-            slide.title.content &&
-            slide.date &&
-            slide.date.content) {
-          // Copy the title and start time
-          newSlide.title = slide.title.content
-          newSlide.start = this.addOneDay(slide.date.content)
-        }
-        return newSlide
-      })
+  data () {
+    return {
+      events: [],
+      config: {
+        defaultView: 'month'
+      }
     }
+  },
+  created () {
+    this.events = this.$store.getters.getAllSlides.map((slide) => {
+      const newSlide = {}
+      // Events at least need title and date information
+      if (slide &&
+          slide.title &&
+          slide.title.content &&
+          slide.date &&
+          slide.date.content) {
+        // Copy the title and start time
+        newSlide.title = slide.title.content
+        newSlide.start = slide.date.content
+        // Now if the event also has time or description, add it into the slide as well. Notice that description is not useful at all in this step, but might be useful later
+        if (slide.description &&
+            slide.description.content) {
+          newSlide.description = slide.description.content
+        }
+        if (slide.time &&
+            slide.time.content) {
+          // Since the event cannot show correctly with the time from "0 AM" to "10 AM",
+          // We have to make a new Date
+          newSlide.start = new Date(newSlide.start + ' ' + slide.time.content)
+        }
+      }
+      return newSlide
+    })
   },
   methods: {
-    // Since the calendar view cannot show the correct date,
-    // One day is added manually
-    addOneDay: function (dateString) {
-      return new Date(dateString).setDate(new Date(dateString).getDate() + 1)
-    }
+
   }
 }
 
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
