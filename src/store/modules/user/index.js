276bb3e558cc8b64b3664f8345b3b45174c92606
Benjamin Korol (bek082) <ben.korol@usask.ca>
2018-03-15 18:41:20 -0600

    add mutation, mutation type and getter for token, signIn

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -1,8 +1,11 @@
-import { authService } from '@/services/firebase.conf'
-import { SET_USER, SET_LOADING } from '@/store/mutation-types'
+import request from 'superagent'
+import { SET_USER, SET_TOKEN, SET_LOADING } from '@/store/mutation-types'
+
+const server = 'https://cmpt371g4.usask.ca:8443'
 
 // state of this module
 const state = {
+  token: '',
   user: { id: '_change', email: 'blah@blah.com' }
 }
 
@@ -10,6 +13,9 @@ const state = {
 const getters = {
   user (state) {
     return state.user
+  },
+  token (state) {
+    return state.token
   },
   isAuthenticated (state) {
     return state.user !== null && state.user !== undefined
@@ -20,6 +26,9 @@ const getters = {
 const mutations = {
   [SET_USER] (state, payload) {
     state.user = payload.user
+  },
+  [SET_TOKEN] (state, payload) {
+    state.token = payload.token
   }
 }
 
@@ -27,32 +36,32 @@ const mutations = {
 const actions = {
   signIn ({ commit }, payload) {
     commit(SET_LOADING, { loading: true })
-    authService.signInWithEmailAndPassword(`${payload.username}@${payload.domain}`, payload.password)
-      .then(
-        user => {
+
+    return new Promise((resolve, reject) => {
+      request.post(server + '/api/login/' + payload)
+        .then(function (responce) {
+          commit(SET_TOKEN, responce)
           const newUser = {
-            id: user.uid,
-            name: user.email
+            id: responce.id,
+            name: responce
           }
           commit(SET_LOADING, { loading: false })
           commit(SET_USER, { 'user': newUser })
-        }
-      )
-      .catch(
-        error => {
+        })
+        .catch(function (err) {
+          reject(err)
           commit(SET_LOADING, { loading: false })
-          console.log(error)
-        }
-      )
+          console.log(err)
+        })
+    })
   },
   signOut () {
-    authService.signOut().then(function () {
-      alert('Logged out')
-    }).catch(
-      error => {
-        console.log(error)
-      }
-    )
+    // authService.signOut().then(function () {
+    //   alert('Logged out')
+    // }).catch(
+    //   error => {
+    //     console.log(error)
+    //   }
   }
 }
 
--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,6 +1,7 @@
 // mutation types
 export const SET_LOADING = 'SET_LOADING'
 export const SET_USER = 'SET_USER'
+export const SET_TOKEN = 'SET_TOKEN'
 export const SET_SLIDE = 'SET_SLIDE'
 export const NEW_SLIDE = 'NEW_SLIDE'
 export const SAVE_SLIDE = 'SET_SLIDE'
