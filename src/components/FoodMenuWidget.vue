ecc40d90b9b6cee5cf8b8eb18689989f81460fd3
Amy Kwan (amk899) <amy.kwan@usask.ca>
2018-03-16 12:56:15 -0600

    combine tool bar and tab in one

--- src/components/FoodMenuWidget.vue
+++ src/components/FoodMenuWidget.vue
@@ -1,79 +1,52 @@
 <template>
   <v-container>
-    <div>
+    <v-toolbar
+      dark
+      color="teal lighten-2">
+
+      <v-toolbar-title class="white--text">Food Menu Widget</v-toolbar-title>
+
+      <v-spacer/>
+      <v-btn
+        fab
+        small
+        color="red darken-3"
+        @click.native="addWeek">
+
+        <v-icon dark>add</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>save</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>event</v-icon>
+      </v-btn>
+
       <v-tabs
-        xs12
-        md12
-        sm12
-        lg12
         v-model="active"
-        color="cyan darken-1"
+        align-with-title
+        color="teal lighten-2"
         dark
-        slider-color="yellow">
+        slot="extension"
+        slider-color="amber lighten-2">
+
         <v-tab
           v-for="n in numWeeks"
           :key="n"
           ripple>
           Week {{ n }}
         </v-tab>
-
-        <v-btn
-          fab
-          small
-          color="red lighten-1"
-          @click.native="addWeek">
-
-          <v-icon dark>add</v-icon>
-        </v-btn>
-
-        <v-btn
-          flat
-          fab
-          small>
-
-          <v-icon>save</v-icon>
-        </v-btn>
-
-        <v-btn
-          small
-          fab
-          flat>
-
-          <v-icon dark>event</v-icon>
-        </v-btn>
-
-        <v-tab-item
-          v-for="n in numWeeks"
-          :key="n">
-          <!--
-          <v-card flat>
-            <v-card-text>{{ text }}</v-card-text>
-          </v-card>
-          -->
-          <template>
-
-            <v-layout>
-              <v-flex
-                xs2
-                sm2
-                md2
-                lg2
-                offset-sm2>
-                <v-card>
-                  <v-card-title primary-title>
-                    <div>
-                      <h3 class="headline mb-0">Monday</h3>
-                      <div>testing</div>
-                    </div>
-                  </v-card-title>
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </template>
-
-        </v-tab-item>
       </v-tabs>
-    </div>
+    </v-toolbar>
+
+    <v-tab-item
+      v-for="n in numWeeks"
+      :key="n">
+
+      <v-card flat>
+        <v-card-text v-text="text"/>
+      </v-card>
+    </v-tab-item>
   </v-container>
 </template>
 
@@ -96,7 +69,6 @@ export default {
     }
   }
 }
-
 </script>
 
 <style scoped>
