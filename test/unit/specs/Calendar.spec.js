b93936af22f5a112c264b5ddb2061ec09cfb4f5d
Ehtasham <ehm011@mail.usask.ca>
2018-03-15 18:40:12 -0600

    add more test cases for the Events property

--- test/unit/specs/Calendar.spec.js
+++ test/unit/specs/Calendar.spec.js
@@ -174,6 +174,11 @@ describe.only('Calendar.vue', function () {
       ]
     })
 
+    afterEach(function () {
+      vm.$store.allSlides = []
+      console.log('running after')
+    })
+
     it('should have no slides', function (done) {
       Vue.nextTick(() => {
         expect(vm.Events.length).to.equal(0)
@@ -181,15 +186,124 @@ describe.only('Calendar.vue', function () {
       })
     })
 
-    it('should have 1 slide', function (done) {
+    it('should have correct titles', function (done) {
       vm.$store.state.allSlides = mockSlides
+
       Vue.nextTick(() => {
-        console.log(vm.$store.state.getAllSlides)
+        vm.Events.forEach((event, i) => {
+          expect(event.title).to.equal(mockSlides[i].title.content)
+        })
+        done()
+      })
+    })
+
+    it('should have correct start dates', function (done) {
+      vm.$store.state.allSlides = mockSlides
+
+      Vue.nextTick(() => {
+        vm.Events.forEach((event, i) => {
+          expect(event.start).to.equal(vm.addOneDay(mockSlides[i].date.content))
+        })
+        done()
+      })
+    })
+
+    it('should have correct start dates', function (done) {
+      vm.$store.state.allSlides = mockSlides
+
+      Vue.nextTick(() => {
+        vm.Events.forEach((event, i) => {
+          expect(event.start).to.equal(vm.addOneDay(mockSlides[i].date.content))
+        })
+        done()
+      })
+    })
+
+    it('should be empty given empty slide', function (done) {
+      vm.$store.state.allSlides = [{}]
+
+      Vue.nextTick(() => {
         expect(vm.Events.length).to.equal(1)
+        expect(isEmptyObject(vm.Events[0])).to.equal(true)
+        done()
+      })
+    })
+
+    it('should be empty given empty title', function (done) {
+      vm.$store.state.allSlides = [{
+        title: {},
+        date: {
+          content: '2018-02-15'
+        }
+      }]
+
+      Vue.nextTick(() => {
+        expect(vm.Events.length).to.equal(1)
+        expect(isEmptyObject(vm.Events[0])).to.equal(true)
+        done()
+      })
+    })
+
+    it('should be empty given no title content property', function (done) {
+      vm.$store.state.allSlides = [{
+        title: {
+          fontColor: 'Red',
+          fontSize: 'Large',
+          fontStyle: 'Normal',
+          fontWeight: 'Bold'
+        },
+        date: {
+          content: '2018-02-15'
+        }
+      }]
+
+      Vue.nextTick(() => {
+        expect(vm.Events.length).to.equal(1)
+        expect(isEmptyObject(vm.Events[0])).to.equal(true)
+        done()
+      })
+    })
+
+    it('should be empty given empty date', function (done) {
+      vm.$store.state.allSlides = [{
+        title: {
+          content: 'The Last One'
+        },
+        date: {}
+      }]
+
+      Vue.nextTick(() => {
+        expect(vm.Events.length).to.equal(1)
+        expect(isEmptyObject(vm.Events[0])).to.equal(true)
+        done()
+      })
+    })
+
+    it('should be empty given no date content property', function (done) {
+      vm.$store.state.allSlides = [{
+        title: {
+          content: 'The Last One'
+        },
+        date: {
+          fontColor: 'Red',
+          fontSize: 'Large',
+          fontStyle: 'Normal',
+          fontWeight: 'Bold'
+        }
+      }]
+
+      Vue.nextTick(() => {
+        expect(vm.Events.length).to.equal(1)
+        expect(isEmptyObject(vm.Events[0])).to.equal(true)
         done()
       })
     })
   })
+
+  // checks if obj is an empty Object i.e. obj = {}
+  function isEmptyObject (obj) {
+    return Object.keys(obj).length === 0 && obj.constructor === Object
+  }
 
   describe('Test addOnDay method', function () {
     before(function () {
