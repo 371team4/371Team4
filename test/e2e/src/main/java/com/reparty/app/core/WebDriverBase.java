c86d462a62517dd3062e5e1448b3d6715001f803
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-16 20:16:43 -0600

    clean up webDriverBase

--- test/e2e/src/main/java/com/reparty/app/core/WebDriverBase.java
+++ test/e2e/src/main/java/com/reparty/app/core/WebDriverBase.java
@@ -1,15 +1,17 @@
 package com.reparty.app.core;
 
-import com.reparty.app.utils.CommonUtils;
-import com.reparty.services.DriverService;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
+import com.reparty.app.utils.CommonUtils;
+import com.reparty.services.DriverService;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExternalResource;
@@ -27,28 +29,78 @@ public class WebDriverBase {
   private static final String className = CommonUtils.getCallingClassName();
   private static final Logger logger = LogManager.getLogger(className);
 
+  // the webdriver and the service are there for the lifetime of this class
   public static DriverService driverService;
-  public static WebDriver driver;
-
-  // filepath is ...371Team4/target/classes/../../screenshots/
-  protected final String screenshotsPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "../../screenshots/";
-
+  public static WebDriver webDriver;
 
   @ClassRule
   public static ExternalResource resource = new ExternalResource() {
-    @Override
     // setup webdriver before running a class of tests
-    protected void before() throws Throwable {
+    @Override
+    protected void before() {
+      // this will get executed before the each test case
       driverService = new DriverService();
-      driver = driverService.getChromeDriver();
+      webDriver = driverService.getChromeDriver();
     };
 
-    @Override
     // teardown webdriver after running a class of tests
+    @Override
     protected void after() {
       driverService.releaseWebDriver();
     };
   };
+
+  @Before
+  public void setupBeforeTest(){
+    // start the driver before every test
+    if (webDriver == null) {
+      webDriver = driverService.getChromeDriver();
+    }
+  }
+
+  @After
+  public void tearDownAfterTest() {
+    // shutdown the driver after every testcase
+    driverService.releaseWebDriver();
+    webDriver = null;
+  }
+
+  /**
+   * @return the current instance of the running webDriver
+   */
+  protected final WebDriver getWebDriver() {
+    return webDriver;
+  }
+
+  /**
+   * @return the path string to where the screenshots will be stored
+   */
+  private final String getScreenshotDirectoryPath() {
+    // filepath is ...371Team4/target/classes/../../screenshots/
+    String ssDirPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()
+        .concat("../../screenshots/");
+    File dir = new File(ssDirPath);
+    if (!dir.exists()) {
+      dir.mkdirs();
+    }
+    return dir.toPath().toString();
+  }
+
+  /**
+   * @param Discription of running java method
+   * @return String of screenshot file name
+   */
+  private String getScreenshotName(Description description) {
+    String className = description.getTestClass().getSimpleName();
+    String methodName = description.getMethodName();
+
+    Capabilities capabilities = ((RemoteWebDriver) getWebDriver()).getCapabilities();
+    String os = System.getProperty("os.name").replaceAll(" ", "-");
+    String browser = capabilities.getBrowserName().toLowerCase();
+    String v = capabilities.getVersion().toString();
+    String fileName = String.format("%s-%s-%s_%s.%s().png", os, browser, v, className, methodName);
+    return fileName;
+  }
 
   @Rule
   public final TestRule watchman = new TestWatcher() {
@@ -57,24 +109,12 @@ public class WebDriverBase {
     protected void failed(Throwable e, Description description) {
       logger.traceEntry();
 
-      String className = description.getTestClass().getSimpleName();
-      String methodName = description.getMethodName();
-
-      Capabilities capabilities = ((RemoteWebDriver) driver).getCapabilities();
-      String os = System.getProperty("os.name").replaceAll(" ", "-");
-      String browser = capabilities.getBrowserName().toLowerCase();
-      String v = capabilities.getVersion().toString();
-      String fileName = String.format("%s-%s-%s_%s.%s().png", os, browser, v, className, methodName);
-
       try {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        File dir = new File(screenshotsPath);
-        if (!dir.exists()) dir.mkdirs();
-        File dest = new File(screenshotsPath + fileName);
-
+        File src = ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.FILE);
+        File dest = new File(getScreenshotDirectoryPath(), getScreenshotName(description));
         Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
       } catch (IOException ioe) {
-          throw new RuntimeException(ioe);
+        logger.throwing(new RuntimeException(ioe));
       }
       logger.traceExit();
     }
