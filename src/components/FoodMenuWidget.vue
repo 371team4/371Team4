3c00c33c643a5dacc1691bf13351e3fd1a242913
Amy Kwan (amk899) <amy.kwan@usask.ca>
2018-03-16 18:07:15 -0600

    edit the color of tool bar and debugged size of tabs

--- src/components/FoodMenuWidget.vue
+++ src/components/FoodMenuWidget.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <v-container>
     <v-toolbar
@@ -43,32 +44,105 @@
       v-for="n in numWeeks"
       :key="n">
 
-      <v-card flat>
+       <v-card flat>
         <v-card-text v-text="text"/>
       </v-card>
+
+          <v-layout>
+            <v-flex xs2 sm2 md2 lg2 offset-sm2>
+              <v-card>
+                <v-card-title primary-title>
+                  <div>
+                    <h3 class="headline mb-0">Monday</h3>
+                    <div>testing</div>
+                  </div>
+                </v-card-title>
+              </v-card>
+            </v-flex>
+          </v-layout>
     </v-tab-item>
   </v-container>
 </template>
+-->
+<template>
+  <div>
+    <v-toolbar
+      color="light-blue darken-3"
+      dark
+      tabs>
+      <v-toolbar-title>Food Menu Widget</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon>save</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>event</v-icon>
+      </v-btn>
+      <v-tabs
+        align-with-title
+        color="light-blue darken-3"
+        slot="extension"
+        slider-color="amber lighten-3"
+        v-model="model"
+      >
+        <v-tab
+          v-for="i in 3"
+          :key="i"
+          :href="`#tab-${i}`"
+        >
+          Week {{ i }}
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-tabs-items v-model="model">
+      <v-tab-item
+        v-for="i in 3"
+        :key="i"
+        :id="`tab-${i}`"
+      >
+        <v-card flat>
+          <v-card-text v-text="text"/>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
 
 <script>
+// export default {
+//   data () {
+//     return {
+//       active: null,
+//       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+//       numWeeks: 1
+//     }
+//   },
+//   methods: {
+//     next () {
+//       const active = parseInt(this.active)
+//       this.active = (active < 2 ? active + 1 : 0).toString()
+//     },
+//     addWeek () {
+//       this.numWeeks = this.numWeeks + 1
+//     }
+//   }
+// }
+
 export default {
   data () {
     return {
-      active: null,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      numWeeks: 1
+      numWeeks: 1,
+      model: 'tab-2',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     }
   },
   methods: {
-    next () {
-      const active = parseInt(this.active)
-      this.active = (active < 2 ? active + 1 : 0).toString()
-    },
     addWeek () {
       this.numWeeks = this.numWeeks + 1
     }
   }
 }
+
 </script>
 
 <style scoped>
