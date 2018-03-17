1381275f70945346b3c7e07eceb9f230ebc371d9
Amy Kwan (amk899) <amy.kwan@usask.ca>
2018-03-17 00:12:27 -0600

    add 3 weeks of mock data

--- src/components/FoodMenuWidget.vue
+++ src/components/FoodMenuWidget.vue
@@ -90,6 +90,130 @@ export default {
     }
   },
   computed: {
+    weeks() {
+			return {
+				"Week 1": {
+					Monday: {
+						Lunch: ["Pizza", "Spinach and Peas", "", "", ""],
+						Dinner: ["Meat Loaf", "Chicken Noodle Soup", "", "", ""]
+					},
+					Tuesday: {
+						Lunch: ["Thai Curry", "Strawberry Pancakes", "", "", ""],
+						Dinner: ["Fish Tacos", "Cream of Broccoli Soup", "", "", ""]
+					},
+					Wednesday: {
+						Lunch: ["Chili Prawns", "Fried Rice", "", "", ""],
+						Dinner: ["Beef Wellington", "Sushi", "", "", ""]
+					},
+          Thursday: {
+						Lunch: ["Poutine", "Chicken Burgers", "", "", ""],
+						Dinner: ["Quinoa Salad", "Quesadillas with Chipotle Sauce", "", "", ""]
+					},
+          Friday: {
+						Lunch: ["Banana Crepes", "Beef Stroganoff", "", "", ""],
+						Dinner: ["Peach Cobbler", "Spaghetti and Meatballs", "", "", ""]
+					},
+          Saturday: {
+						Lunch: ["New York Cheesecake", "Spicy Pork Ramen", "", "", ""],
+						Dinner: ["Kimchi Pancakes", "Enchiladas with Salsa Verde", "", "", ""]
+					},
+          Sunday: {
+						Lunch: ["Baked Salmon", "Sirloin Steak", "", "", ""],
+						Dinner: ["Gumbo", "Kale Salad", "", "", ""]
+					}
+				},
+				"Week 2": {
+					Monday: {
+						Lunch: ["Pizza", "Spinach and Peas", "", "", ""],
+						Dinner: ["Meat Loaf", "Chicken Noodle Soup", "", "", ""]
+					},
+					Tuesday: {
+						Lunch: ["Thai Curry", "Strawberry Pancakes", "", "", ""],
+						Dinner: ["Fish Tacos", "Cream of Broccoli Soup", "", "", ""]
+					},
+					Wednesday: {
+						Lunch: ["Chili Prawns", "Fried Rice", "", "", ""],
+						Dinner: ["Beef Wellington", "Sushi", "", "", ""]
+					},
+          Thursday: {
+						Lunch: ["Poutine", "Chicken Burgers", "", "", ""],
+						Dinner: ["Quinoa Salad", "Quesadillas with Chipotle Sauce", "", "", ""]
+					},
+          Friday: {
+						Lunch: ["Banana Crepes", "Beef Stroganoff", "", "", ""],
+						Dinner: ["Peach Cobbler", "Spaghetti and Meatballs", "", "", ""]
+					},
+          Saturday: {
+						Lunch: ["New York Cheesecake", "Spicy Pork Ramen", "", "", ""],
+						Dinner: ["Kimchi Pancakes", "Enchiladas with Salsa Verde", "", "", ""]
+					},
+          Sunday: {
+						Lunch: ["Baked Salmon", "Sirloin Steak", "", "", ""],
+						Dinner: ["Gumbo", "Kale Salad", "", "", ""]
+					}
+				},
+        "Week 3": {
+					Monday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+					Tuesday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+					Wednesday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Thursday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Friday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Saturday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Sunday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					}
+				},
+        "Week 4": {
+					Monday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+					Tuesday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+					Wednesday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Thursday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Friday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Saturday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					},
+          Sunday: {
+						Lunch: ["", "", "", "", ""],
+						Dinner: ["", "", "", "", ""]
+					}
+				},
+			};
+		}
   },
   methods: {
     addWeek () {
