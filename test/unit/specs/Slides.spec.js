85d05f97e1f2e424e584ad562f5fc3eb5a618835
KingMackle <mahmoud.mahmoud@usask.ca>
2018-03-18 00:25:06 -0600

    make test more deterministic

--- test/unit/specs/Slides.spec.js
+++ test/unit/specs/Slides.spec.js
@@ -238,7 +238,7 @@ describe('Slides.vue', () => {
       sinon.spy(vm.$store, 'commit')
       const cardHeader = vm.$el.querySelector('[data-test-attr=\'slideCard_1\'] .headline')
       cardHeader.click()
-      expect(vm.$store.commit.firstCall.args).to.deep.equal(['SET', mockSlides[1]])
+      expect(vm.$store.commit.firstCall.args).to.deep.equal([CURRENT_SLIDE.SET, mockSlides[1]])
     })
 
     it('should navigate to the desginer view', () => {
