b129ed25b8878082c06d5085d023ceb54fc55933
ridwanraji <ror716@mail.usask.ca>
2018-03-20 19:31:00 -0600

    Implement slides functions

--- src/services/API/slides.js
+++ src/services/API/slides.js
@@ -1,49 +1,43 @@
-import axios from 'axios'
-const server = 'https://cmpt371g4.usask.ca:8081'
+import { server } from '@/services/api.endpoint'
 
-function getAllSlide () {
-  axios.get(server + 'api/slides/')
+export function getAllSlides () {
+  debugger
+  return server.get('api/slides/')
 }
 
 // Takes id as payload and gets the slide with the given id
-function getSlide (payload) {
-  const id = payload.currentSlide._id
-  axios.get(server + 'api/slides/' + {
-    _id: id
-  })
+export function getSlide (id) {
+  if (id) {
+    return server.get(`api/slides/${id}`)
+  } else {
+    console.log('Need it for `getSlide`')
+  }
 }
 
 // used for saving both new slides, and edits to existing slides.
-function saveSlide (payload) {
+export function saveSlide (slide) {
   // trys to assign the database key(_id) to id
-  const id = payload.currentSlide._id
-  // checks if id object exists (it does if already in DB) otherwise it doesn't and will be false.
-  if (id) {
-    // If id already exist, post the body of current slide in the id that already exist
-    axios.put(server + '/api/slides/' + id, {
-      body: state.currentSlide })
-    // reloads the window or page
-    window.location.reload()
+  if (slide) {
+    // checks if id object exists (it does if already in DB) otherwise it doesn't and will be false.
+    if (slide._id) {
+      // If id already exist, post the body of current slide in the id that already exist
+      return server.put(`api/slides/${slide._id}`, {
+        body: slide })
+    } else {
+      // id does not exist in the database so we save as new slide
+      return server.post(server + '/api/slides/', {
+        body: slide
+      })
+    }
   } else {
-    // id does not exist in the database so we save as new slide
-    axios.post(server + '/api/slides/', {
-      body: state.currentSlide
-    })
+    console.log('Need it for `saveSlide`')
   }
-
-  // This should not be set to false until it is confirmed that the slide is saved
-  // Because the modifications to the slide have now been saved to database,
-  // both this version, and the database one should be the same, so dirty
-  // state is now false.
 }
 // takes currentSlide as payload, used for deleting slides from database.
-function deleteSlide (payload) {
-  // assign the database key(_id) to id
-  const id = state.currentSlide._id
-
-  axios.delete(server + '/api/slides/' + {
-    id
-  })
-  // reloads the window or page
-  window.location.reload()
+export function deleteSlide (id) {
+  if (id) {
+    return server.delete(`api/slides/${id}`)
+  } else {
+    console.log('Need it for `deleteSlide`')
+  }
 }
