01d2e503361dfe5a8d9c2859411626b322822611
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-20 19:43:38 -0600

    move api endpoint to root folder

--- src/services/API/api.endpoint.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const server = axios.create({
-  baseURL: 'http://cmpt371g4.usask.ca:8081'
-})
---
+++ src/services/api.endpoint.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const server = axios.create({
+  baseURL: 'http://cmpt371g4.usask.ca:8081'
+})
+
+export function setToken (token) {
+  if (token) {
+    server.defaults.headers['x-access-token'] = token
+  } else {
+    console.error('Need token for authentication ')
+  }
+}
