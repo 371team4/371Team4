5a0efba127ccb5c8e0470f81d8d5242bb5625689
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-22 21:21:13 -0600

    fix test cases

--- test/unit/specs/store/Slide.spec.js
+++ test/unit/specs/store/Slide.spec.js
@@ -1,125 +1,104 @@
-import index from '@/store/modules/slide/index'
-// import * as CURRENT_SLIDE from '@/store/modules/slide/mutation-types'
-// import { store } from '@/store'
+import Vue from 'vue'
+import { store } from '@/store'
+import * as CURRENT_SLIDE from '@/store/modules/slide/mutation-types'
 
-// const emptySlide = {
-//   // the title of the slide, what it is called by humans, has text, font information and color.
-//   title: { content: '', fontColor: null, fontSize: null, fontStyle: null, fontWeight: null },
-//   // similar to title, but generally larger text content.
-//   description: { content: '', fontColor: null, fontSize: null, fontStyle: null, fontWeight: null },
-//   // image array of image files (or link to image files?) to be displayed in the slide
-//   images: [],
-//   // the date of the slides event, has similar font info as title/description, content is date object
-//   date: { content: null, fontColor: null, fontSize: null, fontStyle: null, fontWeight: null },
-//   // the time of the event, same as date but time object instead of date object
-//   time: { content: null, fontColor: null, fontSize: null, fontStyle: null, fontWeight: null },
-
-//   meta: { template: null, timeout: '', repeatable: false, startDate: '', endDate: '' }
-// }
-
-let mockSlide = [
-  {
-    images: [
-      {
-        src: 'https://picsum.photos/200/300/?image=92'
-      },
-      {
-        src: 'https://picsum.photos/200/300/?image=19'
-      },
-      {
-        src: 'https://picsum.photos/200/300/?image=14'
-      },
-      {
-        src: 'https://picsum.photos/200/300/?image=94'
-      }
-    ],
-    title: {
-      content: 'SlideAPIUnitTest1',
-      fontColor: 'Blue',
-      fontSize: 'Large',
-      fontStyle: 'Normal',
-      fontWeight: 'Bold'
-    },
-    description: {
-      content: 'This is just for Slide API unit test',
-      fontColor: 'Blue',
-      fontSize: 'Large',
-      fontStyle: 'Normal',
-      fontWeight: 'Bold'
-    },
-    date: {
-      content: '2018-03-21',
-      fontColor: 'Red',
-      fontSize: 'Large',
-      fontStyle: 'Normal',
-      fontWeight: 'Bold'
-    },
-    time: {
-      content: '11:40',
-      fontColor: 'Red',
-      fontSize: 'Large',
-      fontStyle: 'Normal',
-      fontWeight: 'Bold'
-    },
-    meta: {
-      template: 'DefaultSlideTemplate',
-      timeout: '40',
-      repeatable: false,
-      startDate: '2018-03-21',
-      endDate: '2018-03-30'
-    }
-  }
-]
-
-// helper for testing action with expected mutations
-const testAction = (action, args, state, expectedMutations, done) => {
-  let count = 0
-
-  // mock commit
-  const commit = (type, payload) => {
-    const mutation = expectedMutations[count]
-
-    try {
-      expect(mutation.type).to.equal(type)
-      if (payload) {
-        expect(mutation.payload).to.deep.equal(payload)
-      }
-    } catch (error) {
-      done(error)
-    }
-
-    count++
-    if (count >= expectedMutations.length) {
-      done()
-    }
-  }
-
-  // call the action with mocked store and argument
-  action({ commit }, ...args)
-
-  // Check if no mutation should have been dispatched
-  if (expectedMutations.length === 0) {
-    expect(count).to.equal(0)
-    done()
+let newSlide = {
+  images: [],
+  title: {
+    content: 'SlideAPIUnitTest1',
+    fontColor: 'Blue',
+    fontSize: 'Large',
+    fontStyle: 'Normal',
+    fontWeight: 'Bold'
+  },
+  description: {
+    content: 'This is just for Slide API unit test',
+    fontColor: 'Blue',
+    fontSize: 'Large',
+    fontStyle: 'Normal',
+    fontWeight: 'Bold'
+  },
+  date: {
+    content: '2018-03-21',
+    fontColor: 'Red',
+    fontSize: 'Large',
+    fontStyle: 'Normal',
+    fontWeight: 'Bold'
+  },
+  time: {
+    content: '11:40',
+    fontColor: 'Red',
+    fontSize: 'Large',
+    fontStyle: 'Normal',
+    fontWeight: 'Bold'
+  },
+  meta: {
+    template: 'DefaultSlideTemplate',
+    timeout: '40',
+    repeatable: false,
+    startDate: '2018-03-21',
+    endDate: '2018-03-30'
   }
 }
 
+const Constructor = Vue.extend()
+const vm = new Constructor({ store }).$mount()
+
+function waitFor (func, timeout) {
+  setTimeout(() => func(), timeout)
+}
+
 describe('index', function () {
+  before('Setup before slide tests', function (done) {
+    vm.$store.dispatch('signIn', { username: 'test', password: 'admin001' })
+    vm.$store.dispatch('initAllSlides')
+    waitFor(done, 1000)
+  })
+
   it('should getAllSLides', done => {
-    expect(index.state.allSlides).to.be.deep.equal([])
-    testAction(index.actions.initAllSlides, [], {}, [], done)
-    expect(index.state.allSlides.length).to.be.deep.equal([3])
+    vm.$store.dispatch('initAllSlides')
+    waitFor(() => {
+      expect(vm.$store.getters.getAllSlides).to.not.deep.equal([])
+      done()
+    }, 1000)
   })
-})
 
-describe('index', function () {
-  it('should saveSlide', done => {
-    testAction(index.actions.saveSlide, [mockSlide], {}, [], done)
+  it('should saveSlide when it is new', done => {
+    const ogSlide = vm.$store.getters.getAllSlides[0]
+    newSlide.images = [ogSlide.images[0]._id]
+    vm.$store.commit(CURRENT_SLIDE.SET, newSlide)
+    vm.$store.dispatch('saveSlide')
+    waitFor(() => {
+      newSlide._id = ''
+      newSlide.createdAt = ''
+      newSlide.__v = ''
+      expect(vm.$store.getters.getCurrentSlide).to.have.all.keys(...Object.keys(newSlide))
+      done()
+    }, 1000)
   })
-})
 
-describe('index', function () {
+  it.skip('should saveSlide when it is not new', done => {
+    debugger
+    const ogSlide = vm.$store.getters.getAllSlides[0]
+    vm.$store.dispatch('getSlide', ogSlide._id)
+    waitFor(() => {
+      vm.$store.commit(CURRENT_SLIDE.SET_TITLE_CONTENT, 'updatedSlide')
+      vm.$store.dispatch('saveSlide')
+      waitFor(() => {
+        debugger
+        ogSlide.title.content = 'updatedSlide'
+        expect(vm.$store.getters.getCurrentSlide).to.deep.equal(ogSlide)
+        done()
+      }, 1000)
+    }, 1000)
+  })
+
   it('should getSlide', done => {
-    testAction(index.actions.initSlide('5aaeb4523cc35925c0bd6160'), [], {}, [], done)
+    const slide = vm.$store.getters.getAllSlides[0]
+    vm.$store.dispatch('getSlide', slide._id)
+    waitFor(() => {
+      expect(vm.$store.getters.getCurrentSlide).to.deep.equal(slide)
+      done()
+    }, 1000)
   })
 })
