2a626986da836bac718c3f9e3a9923322825411f
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-23 10:17:57 -0600

    add signIn and signOut logic, change mutation type to SET_USER

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,7 +1,6 @@
 // mutation types
 export const SET_LOADING = 'SET_LOADING'
-export const SET_USERNAME = 'SET_USERNAME'
-export const SET_TOKEN = 'SET_TOKEN'
+export const SET_USER = 'SET_USER'
 export const SET_SLIDE = 'SET_SLIDE'
 export const NEW_SLIDE = 'NEW_SLIDE'
 export const SAVE_SLIDE = 'SET_SLIDE'
