b59ed66d218bdbeb0d8613a1d4c3d2a98645f412
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-23 11:52:31 -0600

    add remove token function

--- src/services/api.endpoint.js
+++ src/services/api.endpoint.js
@@ -11,3 +11,7 @@ export function setToken (token) {
     console.error('Need token for authentication ')
   }
 }
+
+export function removeToken () {
+  delete server.defaults.headers['x-access-token']
+}
