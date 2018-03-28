c7be3d0b12d3b9e3ed972cf07bd9627c112a9bcd
Nam Pham <hnp435@mail.usask.ca>
2018-03-27 20:13:26 -0600

    update the current test attributes for some components of Designer.vue

--- test/e2e/src/main/java/com/reparty/app/pageObjs/AuthorSlide.java
+++ test/e2e/src/main/java/com/reparty/app/pageObjs/AuthorSlide.java
@@ -2,8 +2,30 @@ package com.reparty.app.pageObjs;
 
 import com.reparty.app.core.PageObjBase;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class AuthorSlide extends PageObjBase {
+
+  @FindBy(css= "[data-test-attr='authorSlide']")
+  private WebElement AuthorSlide;
+
+  @FindBy(css= "[data-test-attr='title']")
+  private WebElement TitleInput;
+
+  @FindBy(css= "[data-test-attr='addButton']")
+  private WebElement AddButton;
+
+  @FindBy(css= "[data-test-attr='uploadButton']")
+  private WebElement UploadButton;
+
+  @FindBy(css= "[data-test-attr='submitButton']")
+  private WebElement SubmitButton;
+
+  @FindBy(css= "[data-test-attr='previewButton']")
+  private WebElement PreviewButton;
+
+  @FindBy(css= "[data-test-attr='clearButton']")
+  private WebElement ClearButton;
 
   public AuthorSlide(WebElement element) {
     super(element);
