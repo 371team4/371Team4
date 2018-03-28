c430b2a8136505abf0e39895169c36cac43826ca
Nam Pham <hnp435@mail.usask.ca>
2018-03-27 19:53:47 -0600

    make the page objects for AuthorSlide

---
+++ test/e2e/src/main/java/com/reparty/app/pageObjs/AuthorSlide.java
@@ -0,0 +1,13 @@
+package com.reparty.app.pageObjs;
+
+import com.reparty.app.core.PageObjBase;
+import org.openqa.selenium.WebElement;
+
+public class AuthorSlide extends PageObjBase {
+
+  public AuthorSlide(WebElement element) {
+    super(element);
+  }
+
+
+}
