1af4ce79167a43ce5e4bed1bfa94982595e67af8
Mahmoud <mahmoud.mahmoud@usask.ca>
2018-03-29 16:36:33 -0600

    remove vuexfire and upload module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
-import { firebaseMutations } from 'vuexfire'
 
 import root from './root'
 import user from './modules/user'
-import upload from './modules/upload'
 import slide from './modules/slide'
 import image from './modules/image'
 import login from './modules/login'
@@ -16,11 +14,9 @@ const isDebug = process.env.NODE_ENV !== 'production'
 
 export const store = new Vuex.Store({
   strict: isDebug,
-  mutations: firebaseMutations,
   modules: {
     root,
     user,
-    upload,
     slide,
     image,
     login
@@ -30,13 +26,12 @@ export const store = new Vuex.Store({
 
 if (module.hot) {
   // accept actions and mutations as hot modules
-  module.hot.accept(['./root', './modules/user', './modules/slide', './modules/image', './modules/upload','./modules/login'], () => {
+  module.hot.accept(['./root', './modules/user', './modules/slide', './modules/image', './modules/login'], () => {
     // require the updated modules
     // have to add .default here due to babel 6 module output
     const root = require('./root').default
     const user = require('./modules/user').default
     const slide = require('./modules/slide').default
-    const upload = require('./modules/upload').default
     const image = require('./modules/image').default
     const login = require('./modules/login').default
     // swap in the new actions and mutations
@@ -45,7 +40,6 @@ if (module.hot) {
         root,
         user,
         slide,
-        upload,
         image,
         login
       }
