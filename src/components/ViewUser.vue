3693fcb0258f6a66670b0c110fca43f8c53b4fd0
Amy Kwan (amk899) <amy.kwan@usask.ca>
2018-03-31 00:04:32 -0600

    added function stub for save item

--- src/components/ViewUser.vue
+++ src/components/ViewUser.vue
@@ -139,74 +139,20 @@ export default {
     initialize () {
       this.items = [
         {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
+          username: 'Yggep',
+          password: 'olympus'
         },
         {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
+          username: 'Duomham',
+          password: 'kool'
         },
         {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
+          username: 'Kire',
+          password: 'physics'
         },
         {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
+          username: 'Yma',
+          password: 'helloworld'
         }
       ]
     },
@@ -220,6 +166,10 @@ export default {
     deleteItem (item) {
       const index = this.items.indexOf(item)
       confirm('Are you sure you want to delete this item?') && this.items.splice(index, 1)
+    },
+
+    saveItem (item) {
+      console.log('Button was clicked!')
     },
 
     close () {
