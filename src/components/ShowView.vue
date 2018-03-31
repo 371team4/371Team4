8bd3e013a643e62c97e61de5142b7e33a3156381
KingMackle <mahmoud.mahmoud@usask.ca>
2018-03-30 20:04:08 -0600

    interval function for slider

--- src/components/ShowView.vue
+++ src/components/ShowView.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    <h1>Show View</h1>
-  </div>
+  <default-slide-template :slide="slide"/>
 </template>
 
 <script>
+import DefaultSlideTemplate from '@/components/templates/DefaultSlideTemplate'
+import { mapGetters } from 'vuex'
 export default {
-
+  components: {
+    DefaultSlideTemplate
+  },
+  data: () => {
+    return {
+      index: 0
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'allSlides'
+    ]),
+    slide () {
+      return this.allSlides[this.index]
+    }
+  },
+  created () {
+    this.intervalFunction = setInterval(() => {
+      this.index < this.allSlides.length ? this.index++ : this.index = 0
+    }, this.allSlides[this.index].meta.timeout * 1000)
+  }
 }
 </script>
 
