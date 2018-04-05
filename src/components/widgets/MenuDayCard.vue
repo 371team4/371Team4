62e9d7b2f748670cf70fd6a548e4405e3a7e7768
KingMackle <mahmoud.mahmoud@usask.ca>
2018-04-05 01:10:44 -0600

    more style fixes

--- src/components/widgets/MenuDayCard.vue
+++ src/components/widgets/MenuDayCard.vue
@@ -1,43 +1,41 @@
 <template>
-  <v-container>
-    <v-card
-      hover
-      :data-test-attr="testAttr">
-      <!-- to be positioned later -->
-      <v-card-text
-        data-test-attr="dayName"
-        class="pt-2 pb-0">
-        {{ day.name }}
-      </v-card-text>
-      <v-layout
-        wrap
-        horizontal>
-        <v-flex
-          class="mx-1 my-1"
-          v-for="(content, name) in day.meals"
-          :key="name">
-          <!-- need a div to get this data test attr, v-flex and v-container don't accept custom html attrs -->
-          <div :data-test-attr="`mealContainer_${name}`">
-            <v-subheader
-              data-test-attr="mealName"
-              class="justify-center py-1">
-              {{ name }}
-            </v-subheader>
-            <v-text-field
-              solo
-              label="Food Item"
-              class="mb-1"
-              data-test-attr="menuItem"
-              v-for="(menuItem, index) in content"
-              :key="index"
-              :name="menuItem"
-              :readonly="readOnly"
-              :flat="readOnly"/>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-card>
-  </v-container>
+  <v-card
+    hover
+    data-test-attr="menuDayCard">
+    <!-- to be positioned later -->
+    <v-card-text
+      data-test-attr="dayName"
+      class="pt-2 pb-0 headline">
+      {{ day.name }}
+    </v-card-text>
+    <v-layout
+      wrap
+      horizontal>
+      <v-flex
+        class="mx-1 my-1"
+        v-for="(content, name) in day.meals"
+        :key="name">
+        <!-- need a div to get this data test attr, v-flex and v-container don't accept custom html attrs -->
+        <div :data-test-attr="`mealContainer_${name}`">
+          <v-subheader
+            data-test-attr="mealName"
+            class="justify-center py-1">
+            {{ name }}
+          </v-subheader>
+          <v-text-field
+            solo
+            label="Food Item"
+            class="mb-1"
+            data-test-attr="menuItem"
+            v-for="(menuItem, index) in content"
+            :key="index"
+            :name="menuItem"
+            :readonly="readOnly"
+            :flat="readOnly"/>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
@@ -56,11 +54,6 @@ export default {
           'Supper': [' ', ' ', ' ', ' ', ' ']
         }
       })
-    }
-  },
-  data () {
-    return {
-      testAttr: 'menuDayCard'
     }
   }
 }
